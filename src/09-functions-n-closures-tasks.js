006c1ffb9328834c7b828d520249165a1699c6b2
Loshkarev Nikita <loshkarev.n@inbox.ru>
2020-12-15 02:51:45 +0400

    feat: complete last conditions-n-lopps-tasks

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -101,8 +101,18 @@ function memoize(/* func */) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  let tries = attempts;
+  return () => {
+    do {
+      try {
+        return func();
+      } catch (error) {
+        tries -= 1;
+      }
+    } while (tries);
+    return func();
+  };
 }
 
 
@@ -147,8 +157,8 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...args2) => fn(...args1, ...args2);
 }
 
 
@@ -169,8 +179,13 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+
+function getIdGeneratorFunction(startFrom) {
+  let idCounter = startFrom - 1;
+  return () => {
+    idCounter += 1;
+    return idCounter;
+  };
 }
 
 
